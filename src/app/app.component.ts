47cb4e1bbb38b28bdf8c5d0e148078d7a24121df
Talhaitis612 <bsem-f18-206@superior.edu.pk>
2023-01-28 15:14:29 +0500

    using subject to handle error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PostsService } from './posts.service';
 import { Post } from './post.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: any = [];
   isFetching = false;
+  error = null;
+  private errorSub! : Subscription;
 
   constructor(private http: HttpClient, private postService: PostsService) { }
 
   ngOnInit() {
     this.isFetching = true;
+    this.errorSub = this.postService.error.subscribe({
+      next : (errorMessage:any)=>{
+        this.error= errorMessage;
+      }
+    })
     this.postService.fetchPosts().subscribe({
       next: (posts) => {
         this.isFetching = false;
         this.loadedPosts = posts;
-      }
+      },
+      error: (err)=> {
+        console.log(err);
+        this.error = err.error.error;
+      },
     })
   }
 
   onCreatePost(postData: Post) {
-    this.postService.createAndStorePost(postData.title, postData.content).subscribe({
-      next: (res) => {
-
-      },
-      error: (err: Error) => {
-
-      }
-    });
-
+    this.postService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
@@ -54,6 +58,10 @@ export class AppComponent implements OnInit {
         this.loadedPosts = [];
       }
     })
+  }
+
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
   }
 
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
-import {map} from 'rxjs';
+import {map, Subject} from 'rxjs';
 @Injectable({
     providedIn: 'root'
 })
 export class PostsService {
 
-
+    error = new Subject<string>();
     constructor(private http : HttpClient){}
 
     createAndStorePost(title : string, content:string){
         const postData  : Post = {title : title, content: content};
-        return this.http.post('https://recipe-app-f81b0-default-rtdb.firebaseio.com/posts.json', postData);
+        this.http.post('https://recipe-app-f81b0-default-rtdb.firebaseio.com/posts.json', postData).subscribe(
+            {
+                next : (res)=>{},
+                error : (err)=>{
+                    this.error.next(err.message);
+                }
+            }
+        );
     }
 
     fetchPosts(){
